57cf0fe6a3fd6b4657957d25e7a4c886fc477b0d
Rado Chrome <rado.tsvetkov@gmail.com>
2025-03-06 17:23:09 +0100

    n

--- Pneumonia_2025.R
+++ Pneumonia_2025.R
@@ -7,3 +7,9 @@ data <- read_sheet("1Ljk4bDzvt8v4L-iwfQmTkQ6hBe2WWVeHM2m_BVF5-yg",
            sheet = "Vital Sings-Rado-2025-01-07-2025-03-04",
            range = "A1:J57")
 data <- clean_names(data)
+
+min_spO2 <- data$min_spo2_percent
+
+q1 = qcc(min_spO2[1:14], 
+         type="xbar.one", 
+         newdata=min_spO2[15:56])
